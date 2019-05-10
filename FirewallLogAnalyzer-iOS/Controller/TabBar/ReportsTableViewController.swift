13d6781e9b1934543e435ed217e3774840a43545
Vadim Denisov <gidsor@gmail.com>
2019-05-10 15:17:27 +0500

    Create report with charts by filters for kaspersky logs

--- FirewallLogAnalyzer-iOS/Controller/TabBar/ReportsTableViewController.swift
+++ FirewallLogAnalyzer-iOS/Controller/TabBar/ReportsTableViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ReportsTableViewController: UITableViewController {
+    
+    var kasperskyLogs: [KasperskyLog] = []
+    var tplinkLogs: [TPLinkLog] = []
+    var dlinkLogs: [DLinkLog] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.tableFooterView = UIView()
+        
+        update()
+    }
+    
+    func update() {
+        var kasperskyLoaded = false
+        var tplinkLoaded = false
+        var dlinkLoaded = false
+        
+//        showActivityIndicator(in: view)
+        NetworkManager.shared.updateKasperskyLogFiles { (status, logs) in
+            self.kasperskyLogs = logs
+            kasperskyLoaded = true
+            if kasperskyLoaded && tplinkLoaded && dlinkLoaded {
+                self.hideActivityIndicator(in: self.view)
+            }
+        }
+        
+        NetworkManager.shared.updateTPLinkLogFiles { (status, logs) in
+            self.tplinkLogs = logs
+            tplinkLoaded = true
+            if kasperskyLoaded && tplinkLoaded && dlinkLoaded {
+                self.hideActivityIndicator(in: self.view)
+            }
+        }
+        
+        NetworkManager.shared.updateDLinkLogFiles { (status, logs) in
+            self.dlinkLogs = logs
+            dlinkLoaded = true
+            if kasperskyLoaded && tplinkLoaded && dlinkLoaded {
+                self.hideActivityIndicator(in: self.view)
+            }
+        }
     }
 }
